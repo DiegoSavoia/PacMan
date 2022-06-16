b7aefda0f5a009010e0dcfffbbde0fb10551101b
Diego Savoia <diegosavoia@epet12smandes.edu.ar>
2022-06-16 13:31:05 -0300

    Creadas carpetas para el html y el css

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
     }
   })
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('html/index.html')
 
   ipcMain.on("minimizeApp", () => {
     mainWindow.minimize()
