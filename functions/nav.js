154875b8378495d2357865371b12cb61450f6855
Diego <diegosavoia@epet12smandes.edu.ar>
2022-06-23 12:58:58 -0300

    score.html creado NOTA: errores en su css

--- functions/nav.js
+++ functions/nav.js
@@ -64,7 +64,7 @@ fetch('../components/nav.html')
     }
 
     function redirectScore() {
-        location.replace("menu-final.html")
+        location.replace("score.html")
     }
     function redirectGame() {
         location.replace("game.html")
