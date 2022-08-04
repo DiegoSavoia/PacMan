919ac84339ccc63b4ebc4c70d8f8ec97e8cf2c50
Diego <diegosavoia@epet12smandes.edu.ar>
2022-08-04 10:51:13 -0300

    Implementada funcionalidad de lista para los nombres de el ranking del menú score

--- functions/list-function.js
+++ functions/list-function.js
@@ -1,11 +1,22 @@
 var scoreList = [2350, 215, 6273, 25, 1000, 234, 4];
+var playerList = ["Carlos","Juli","Sose"]
 
 const scoreRank = document.getElementById("score-rank")
+const playerRank = document.getElementById("score-player")
 
 function scoreToHTML(score, index){
     return `
         <li id="score${index}" class="scores">
             <p>${score}</p>
+        </li>
+    `
+}
+
+
+function playerToHTML(player, index){
+    return `
+        <li id="player${index}" class="scores">
+            <p>${player}</p>
         </li>
     `
 }
@@ -18,4 +29,14 @@ function renderRank() {
   })
 }
 
+
+function renderPlayer() {
+    playerRank.innerHTML = "";
+
+    playerList.forEach((player, index) => {
+    playerRank.innerHTML += playerToHTML(player, index);
+  })
+}
+
 renderRank()
+renderPlayer()
