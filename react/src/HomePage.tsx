787b503cfab4f8ff2360f6bda1a441d10fe6ba8d
Celi <celinaburunov@epet12smandes.edu.ar>
2022-09-08 11:56:17 -0300

    Modularización de homepage

--- react/src/HomePage.tsx
+++ react/src/HomePage.tsx
@@ -12,20 +12,18 @@ function HomePage(){
         throw new Error("Function not implemented.")
     }
     return(
-        
-        <div id="menu-inicio">
-        <img id="logo" src="../img/logo.png"/>
-
-        <h3 id="menu">Menu</h3>
-
-        <div id="options">
-            <h6 className="button zoom" id="play" onClick= {e=> redirectLogin()} >Play</h6>
-            <h6 className="button zoom" id="score" onClick={e=> redirectScore()}>Score</h6>
-            <h6 className="button zoom" id="skin" onClick={e=> redirectSkins()}>Skin</h6>
-            <h6 className="button zoom" id="exit">Exit</h6>
-        </div>           
-
-    </div>
+        <div className="Homepage">
+            <div id="menu-inicio">
+                <img id="logo" src="../img/logo.png"/>
+                    <h3 id="menu">Menu</h3>
+                        <div id="options">
+                            <h6 className="button zoom" id="play" onClick= {e=> redirectLogin()} >Play</h6>
+                            <h6 className="button zoom" id="score" onClick={e=> redirectScore()}>Score</h6>
+                            <h6 className="button zoom" id="skin" onClick={e=> redirectSkins()}>Skin</h6>
+                            <h6 className="button zoom" id="exit">Exit</h6>
+                        </div>           
+            </div>
+        </div>
     )
 
 }
