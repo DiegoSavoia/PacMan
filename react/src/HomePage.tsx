72246b9090bccfdba6118927e5416dc988835991
Martina Coria <martinacoria@epet12smandes.edu.ar>
2022-10-27 11:12:13 -0300

    Añadida funcionalidad botón Exit en HomePage

--- react/src/HomePage.tsx
+++ react/src/HomePage.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import "./homePage.css"
 import logo from './public/img/logo.png';
+
+const ipcRenderer = window.require("electron").ipcRenderer
+
 function HomePage() {
     const navigate = useNavigate()
 
@@ -8,7 +11,9 @@ function HomePage() {
         throw new Error("Function not implemented.")
     }
     return (
+        
         <div className="Homepage">
+            <script src="directionalnavigation-1.0.0.0.js"></script>
             <div id="menu-inicio">
                 <img id="logo" src="../img/logo.png" />
                 <h3 id="menu">Menu</h3>
@@ -16,7 +21,7 @@ function HomePage() {
                     <h6 className="button zoom" id="play" onClick={e => navigate("/login")} >Play</h6>
                     <h6 className="button zoom" id="score" onClick={e => navigate("/score")}>Score</h6>
                     <h6 className="button zoom" id="skin" onClick={e => navigate("/menuskins")}>Skin</h6>
-                    <h6 className="button zoom" id="exit">Exit</h6>
+                    <h6 className="button zoom" id="exit" onClick={e => ipcRenderer.send('closeApp')}>Exit</h6>
                 </div>
             </div>
         </div>
